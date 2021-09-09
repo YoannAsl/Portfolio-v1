58ee706994b8a3e9a85f332fb4ea9a859becf03b
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-09 23:36:41 +0200

    Added more project descrtiptions

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,76 +2,76 @@ import { useState } from 'react';
 import axios from 'axios';
 
 interface Props {
-	name: string;
-	submitButtonContent: string;
-	formRef: HTMLFormElement;
+    name: string;
+    submitButtonContent: string;
+    formRef: HTMLFormElement;
 }
 
 const Form = ({ name, submitButtonContent, formRef }: Props) => {
-	const [inputs, setInputs] = useState({
-		name: '',
-		email: '',
-		message: '',
-	});
+    const [inputs, setInputs] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
 
-	const handleOnChange = (e) => {
-		setInputs((prev) => ({
-			...prev,
-			[e.target.name]: e.target.value,
-		}));
-	};
+    const handleOnChange = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			await axios.post('https://formspree.io/f/mnqlbvpo', inputs);
-			setInputs({ name: '', email: '', message: '' });
-		} catch (error) {
-			console.error(error);
-		}
-	};
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('https://formspree.io/f/mnqlbvpo', inputs);
+            setInputs({ name: '', email: '', message: '' });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-	return (
-		<form
-			name='contact-form'
-			className='flex flex-col w-full mb-72 px-9 max-w-[600px] mx-auto'
-			onSubmit={handleSubmit}
-			ref={formRef}
-		>
-			<label htmlFor='name'>{name}</label>
-			<input
-				type='text'
-				name='name'
-				className='form-input'
-				onChange={handleOnChange}
-				value={inputs.name}
-			/>
+    return (
+        <form
+            name='contact-form'
+            className='flex flex-col w-full mb-72 px-9 max-w-[600px] mx-auto'
+            onSubmit={handleSubmit}
+            ref={formRef}
+        >
+            <label htmlFor='name'>{name}</label>
+            <input
+                type='text'
+                name='name'
+                className='form-input'
+                onChange={handleOnChange}
+                value={inputs.name}
+            />
 
-			<label htmlFor='email'>Email</label>
-			<input
-				type='email'
-				name='email'
-				className='form-input'
-				onChange={handleOnChange}
-				value={inputs.email}
-			/>
+            <label htmlFor='email'>Email</label>
+            <input
+                type='email'
+                name='email'
+                className='form-input'
+                onChange={handleOnChange}
+                value={inputs.email}
+            />
 
-			<label htmlFor='message'>Message</label>
-			<textarea
-				name='message'
-				className='form-input'
-				onChange={handleOnChange}
-				value={inputs.message}
-			></textarea>
+            <label htmlFor='message'>Message</label>
+            <textarea
+                name='message'
+                className='form-input'
+                onChange={handleOnChange}
+                value={inputs.message}
+            />
 
-			<button
-				type='submit'
-				className='self-center px-5 py-1 text-xl text-white rounded-md bg-primaryblue'
-			>
-				{submitButtonContent}
-			</button>
-		</form>
-	);
+            <button
+                type='submit'
+                className='self-center px-5 py-1 text-xl text-white rounded-md bg-primaryblue'
+            >
+                {submitButtonContent}
+            </button>
+        </form>
+    );
 };
 
 export default Form;
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,63 +11,63 @@ import Form from '../components/Form';
 import Project from '../components/Project';
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
-	props: {
-		...(await serverSideTranslations(locale, ['common'])),
-	},
+    props: {
+        ...(await serverSideTranslations(locale, ['common'])),
+    },
 });
 
 export default function Home({ _nextI18Next }: any) {
-	const locale = _nextI18Next.initialLocale;
-	const { t } = useTranslation('common');
-	const formRef = useRef<HTMLFormElement>(null);
-	// const name = 'Yoann';
+    const locale = _nextI18Next.initialLocale;
+    const { t } = useTranslation('common');
+    const formRef = useRef<HTMLFormElement>(null);
+    // const name = 'Yoann';
 
-	const handleButtonClick = () => {
-		formRef.current.scrollIntoView({ behavior: 'smooth' });
-	};
+    const handleButtonClick = () => {
+        formRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
 
-	return (
-		<div className='text-gray-700'>
-			<Head>
-				{locale == 'en' ? (
-					<title>Yoann Agésilas | Front-End Developer</title>
-				) : (
-					<title>Yoann Agésilas | Développeur Front-End</title>
-				)}
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+    return (
+        <div className='text-gray-700'>
+            <Head>
+                {locale == 'en' ? (
+                    <title>Yoann Agésilas | Front-End Developer</title>
+                ) : (
+                    <title>Yoann Agésilas | Développeur Front-End</title>
+                )}
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-			<LanguageSelector />
+            <LanguageSelector />
 
-			<header className='flex flex-col justify-around max-w-[1440px] h-screen py-20 font-light px-11 sm:py-32 sm:px-24 mx-auto'>
-				<h1 className='mb-20 text-3xl lg:text-4xl'>
-					{t('hero-title')} 👋
-				</h1>
-				<h2 className='max-w-2xl mb-16 text-3xl leading-snug lg:text-4xl lg:leading-normal'>
-					{t('hero-content')}
-				</h2>
-				<button
-					className='px-3 py-2 text-xl text-white bg-blue-500 rounded-md max-w-min whitespace-nowrap lg:text-2xl lg:px-4'
-					onClick={handleButtonClick}
-				>
-					{t('hero-button')}
-				</button>
-			</header>
+            <header className='flex flex-col justify-around max-w-[1440px] h-screen py-20 font-light px-11 sm:py-32 sm:px-24 mx-auto'>
+                <h1 className='mb-20 text-3xl lg:text-4xl'>
+                    {t('hero-title')} 👋
+                </h1>
+                <h2 className='max-w-2xl mb-16 text-3xl leading-snug lg:text-4xl lg:leading-normal'>
+                    {t('hero-content')}
+                </h2>
+                <button
+                    className='px-3 py-2 text-xl text-white bg-blue-500 rounded-md max-w-min whitespace-nowrap lg:text-2xl lg:px-4'
+                    onClick={handleButtonClick}
+                >
+                    {t('hero-button')}
+                </button>
+            </header>
 
-			<section className='section'>
-				<h1 className='section-title'>{t('skills-title')}</h1>
-				<div className='section-content'>
-					<ul className='col-span-2 text-[.9rem] skills-list'>
-						{skills.map((skill, index) => (
-							<li key={index} className='mb-1'>
-								{skill}
-							</li>
-						))}
-					</ul>
-				</div>
-			</section>
+            <section className='section'>
+                <h1 className='section-title'>{t('skills-title')}</h1>
+                <div className='section-content'>
+                    <ul className='col-span-2 text-[.9rem] skills-list'>
+                        {skills.map((skill, index) => (
+                            <li key={index} className='mb-1'>
+                                {skill}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </section>
 
-			{/* <section className='section'>
+            {/* <section className='section'>
 				<h1 className='section-title'>{t('featured-projects')}</h1>
 				<div className='section-content'>
 					<ul>
@@ -89,36 +89,36 @@ export default function Home({ _nextI18Next }: any) {
 				</div>
 			</section> */}
 
-			<section className='section'>
-				<h1 className='section-title'>Projects</h1>
-				<div className='section-content'>
-					<ul>
-						{projects.map((project, index) =>
-							locale == 'en' ? (
-								<Project
-									key={index}
-									title={project.title}
-									description={project.description.en}
-									tags={project.tags}
-								/>
-							) : (
-								<Project
-									key={index}
-									title={project.title}
-									description={project.description.fr}
-									tags={project.tags}
-								/>
-							)
-						)}
-					</ul>
-				</div>
-			</section>
+            <section className='section'>
+                <h1 className='section-title'>Projects</h1>
+                <div className='section-content'>
+                    <ul>
+                        {projects.map((project, index) =>
+                            locale == 'en' ? (
+                                <Project
+                                    key={index}
+                                    title={project.title}
+                                    description={project.description.en}
+                                    tags={project.tags}
+                                />
+                            ) : (
+                                <Project
+                                    key={index}
+                                    title={project.title}
+                                    description={project.description.fr}
+                                    tags={project.tags}
+                                />
+                            )
+                        )}
+                    </ul>
+                </div>
+            </section>
 
-			<Form
-				name={t('form-name')}
-				submitButtonContent={t('form-submit')}
-				formRef={formRef}
-			/>
-		</div>
-	);
+            <Form
+                name={t('form-name')}
+                submitButtonContent={t('form-submit')}
+                formRef={formRef}
+            />
+        </div>
+    );
 }
