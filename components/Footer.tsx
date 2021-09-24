4206f30bbbfc0ffd5399a75fd1421af6fade4e01
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-24 20:32:33 +0200

    Created global Tailwind class for social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,12 @@ const Footer = () => {
         <footer className='flex pb-12 px-9 justify-center'>
             <a
                 href='https://www.linkedin.com/in/yoannagesilas/'
-                className='w-6 h-6 mx-2'
+                className='social-link'
             >
                 <img src='/images/linkedin.svg' alt='linkedin logo' />
             </a>
 
-            <a href='https://github.com/YoannAsl' className='w-6 h-6 mx-2'>
+            <a href='https://github.com/YoannAsl' className='social-link'>
                 <img src='/images/github.svg' alt='github logo' />
             </a>
         </footer>
